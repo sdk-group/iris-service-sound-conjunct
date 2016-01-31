17b0c27b391ff6711c5aeeb72492be33e336cda9
YuuKohaku <heimaolee@gmail.com>
2016-01-31 19:22:48 +0600

    upd

--- src/SoundConjunct/sound-conjunct.js
+++ src/SoundConjunct/sound-conjunct.js
@@ -1,21 +1,41 @@
 'use strict'
 
 let emitter = require("global-queue");
+let sound_util = require('sound-conjunct');
+let path = require('path');
 
 class SoundConjunct {
 	constructor() {
 		this.emitter = emitter;
 	}
 
-	init() {}
+	init({
+		sound_params, output_directory, theme_params
+	}) {
+		let def_opts = {
+			bits: 16,
+			rate: 24000,
+			voice_pause: 200
+		};
+
+		this.theme_folder = path.resolve(__dirname, "../themes");
+		this.sound_params = _.assignWith(def_opts, sound_params, (objValue, srcValue) => {
+			return _.isUndefined(objValue) ? srcValue : objValue;
+		});
+
+		this.output_directory = output_directory || __dirname;
+	}
 
 	//API
 	actionMakePhrase({
 		sound_names,
+		sound_theme,
 		outname
 	}) {
-		var formats = ["oga", "m4a", "mp3", "ogg", "aac"];
-		return transcode(sound_names, path.resolve(__dirname, outname), formats);
+		let fnames = _.map(sound_names, (name) => path.resolve(this.theme_folder, sound_theme, name));
+		let out = path.resolve(this.output_directory, outname);
+		console.log("FIN FNAMES", fnames, out);
+		return concat(fnames, out, this.sound_params);
 	}
 
 
